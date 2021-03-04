984356d7877d8aa357be925cca0233c38c0fa7c4
Salaah Amin <salaah_amin@hotmail.co.uk>
2021-03-04 23:54:49 +0000

    updated main shell script ot use args and not the default vals

--- website_pinger.sh
+++ website_pinger.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 
 # Try to ping the server, if it fails, then send out an email.
-PING_FAILED=$(ping -c 1 bluishpinks.com | grep "100% packet loss" | wc -l)
-if [ $PING_FAILED -eq 1 ]
-then
-  ./send_email_alert.py
+PING_FAILED=$(ping -c 1 $1 | grep "100% packet loss" | wc -l)
+if [ $PING_FAILED -eq 1 ]; then
+  ./send_email_alert.py "${@:2}"
 fi
-echo test > test.txt
