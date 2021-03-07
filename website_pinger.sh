7034ace1574bcfe99a063318d3213881afd9c0fb
Salaah Amin <salaah_amin@hotmail.co.uk>
2021-03-07 01:41:29 +0000

    switched from a ping to a curl command

--- website_pinger.sh
+++ website_pinger.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-# Try to ping the server, if it fails, then send out an email.
-PING_FAILED=$(ping -c 1 $1 | grep "100% packet loss" | wc -l)
-if [ $PING_FAILED -eq 1 ]; then
+# Try to curl the server, if it fails, then send out an email.
+RESPONSE_CODE=$(curl -s -o /dev/null -I -w "%{http_code}" $1)
+if [ $RESPONSE_CODE -ne 200 ]; then
   ./send_email_alert.py "${@:2}"
 fi
