221e209fa98eacfc60b95f12e1ad96cadca09a45
Ubuntu <ubuntu@ip-172-31-30-52.eu-west-2.compute.internal>
2021-03-10 17:26:26 +0000

    updated user agent on curl command.

--- website_pinger.sh
+++ website_pinger.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Try to curl the server, if it fails, then send out an email.
-RESPONSE_CODE=$(curl -s -o /dev/null -I -w "%{http_code}" $1)
+RESPONSE_CODE=$(curl -s -o /dev/null -I -w "%{http_code}" -A "Website Pinger" $1)
 if [ $RESPONSE_CODE -ne 200 ]; then
   ./send_email_alert.py "${@:2}"
 fi
